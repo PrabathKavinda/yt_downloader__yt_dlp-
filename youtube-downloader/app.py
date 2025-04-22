8354132aa81375ed8f2de7b15f5feb78aaa9e308
Prabath Kavinda <pkwellappili@gmail.com>
2025-04-22 21:22:45 +0530

    port re settled to 8080

--- youtube-downloader/app.py
+++ youtube-downloader/app.py
@@ -36,4 +36,4 @@ def download_file(filename):
     )
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
